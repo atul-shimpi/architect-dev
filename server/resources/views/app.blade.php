127cb3a76468b29a7813f9682d806809ccceb28c
Vebto <vebtolabs@gmail.com>
2018-03-09 18:43:21 +0200

    no message

--- server/resources/views/app.blade.php
+++ server/resources/views/app.blade.php
@@ -8,7 +8,7 @@
 
 @section('angular-scripts')
     {{--angular scripts begin--}}
-		<script type="text/javascript" src="client/inline.f6d3941b03c95d245252.bundle.js"></script>
+		<script type="text/javascript" src="client/inline.cde2031b46ebee9abe36.bundle.js"></script>
 		<script type="text/javascript" src="client/polyfills.e7acad7faff0d20b29f4.bundle.js"></script>
 		<script type="text/javascript" src="client/main.29c67dfa47dd6713b3a9.bundle.js"></script>
 	{{--angular scripts end--}}
